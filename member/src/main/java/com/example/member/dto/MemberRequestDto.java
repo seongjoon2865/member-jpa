fc6168af9330610d0403e3418c706ef5687c69c0
seongjoon2865 <cusk09@gmail.com>
2025-02-07 21:18:24 +0900

    Delete member directory

--- member/src/main/java/com/example/member/dto/MemberRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.member.dto;
-
-import lombok.Getter;
-
-@Getter
-public class MemberRequestDto {
-
-    private String content;
-}
--- member/src/main/java/com/example/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.member.repository;
-
-import com.example.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
